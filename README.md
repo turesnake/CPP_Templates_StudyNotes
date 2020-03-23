d2ea553397f8e88a9007ff6d2129a2a6017fe9c9
turesnake <turesnake@163.com>
2020-03-23 11:28:13 +0800

    fst commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# CPP_Templates_StudyNotes
+有关 C++ templates 的学习笔记
+
+本项目不支持编译，仅仅是一份 **代码展示** 和 **用法说明**。
+
+
---
+++ src/pch/pch.h
@@ -0,0 +1,31 @@
+/*
+ * =========================== pch.h ==========================
+ *                          -- tpr --
+ *                                        CREATE -- 2020.03.09
+ *                                        MODIFY -- 
+ * ----------------------------------------------------------
+ */
+#ifndef TPR_PRE_COMPILE_HEADS_H
+#define TPR_PRE_COMPILE_HEADS_H
+
+//--- glm - 0.9.9.5 ---
+//#include "glm_no_warnings.h"
+//#include "fmt/format.h"
+
+//-------------------- CPP --------------------//
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <memory>
+#include <functional>
+#include <optional>
+
+#include <cmath>
+
+
+
+#endif
+
